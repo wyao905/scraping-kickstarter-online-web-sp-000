a2482634592c3f1aecfbd840187050a90ca567b2
Warren Yao <warrenyca905@gmail.com>
2019-06-15 21:11:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,9 @@
 # require libraries/modules here
+require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
