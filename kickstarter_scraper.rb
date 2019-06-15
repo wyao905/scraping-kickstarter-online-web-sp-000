8c7b87db542194ea1d08f714fe902cb1b1ee3ebc
Warren Yao <warrenyca905@gmail.com>
2019-06-15 22:05:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,7 @@ require 'nokogiri'
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("")
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
