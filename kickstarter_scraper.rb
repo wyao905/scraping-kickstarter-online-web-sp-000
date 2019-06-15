2ca15755dacc411f4acfb6cdb5ab979fd7f87db9
Warren Yao <warrenyca905@gmail.com>
2019-06-15 21:20:16 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 require 'nokogiri'
 
 #projects: kickstarter.css("li.project.grid_4")
-#title
+#title: project.css("h2.bbcard_name strong a").text
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
