e3cfd4c7066f64d641617189df31548106988c6d
Warren Yao <warrenyca905@gmail.com>
2019-06-15 22:05:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ require 'nokogiri'
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# location: project.css("")
+# location: project.css("ul.project-meta")
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
